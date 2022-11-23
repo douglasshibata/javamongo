bd179338904022183aa498c0a9b0e77214c34228
Douglas Shibata <douglas.shibata11@gmail.com>
2022-11-23 12:41:54 -0300

    Replace project, and creating a spring boot web project

---
+++ src/main/java/br/com/alura/escolaalura/javamongo/JavamongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.escolaalura.javamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/escolaalura/javamongo/JavamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.escolaalura.javamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
