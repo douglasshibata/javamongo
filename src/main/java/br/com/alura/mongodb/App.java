2015e6081ddd1911eb179f92846814645d3bd0b0
Douglas Shibata <douglas.shibata11@gmail.com>
2022-11-23 12:15:04 -0300

    Insert one, Inserindo um documento

--- src/main/java/br/com/alura/mongodb/App.java
+++ src/main/java/br/com/alura/mongodb/App.java
@@ -1,5 +1,8 @@
 package br.com.alura.mongodb;
 
+import java.util.Arrays;
+import java.util.Date;
+
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
@@ -15,6 +18,15 @@ public class App {
         MongoClient cliente = new MongoClient();
         MongoDatabase bancoDeDados = cliente.getDatabase("test");
         MongoCollection<Document> alunos = bancoDeDados.getCollection("alunos");
+        Document novoAluno = new Document("nome", "Joao")
+                .append("data_nascimento", new Date(2003, 10, 10))
+                .append("curso", new Document("nome", "Historia"))
+                .append("notas", Arrays.asList(10, 9, 8))
+                .append("habilidades", Arrays.asList(
+                        new Document().append("nome", "Ingles").append("nível", "Básico"),
+                        new Document().append("nome", "Espanhol").append("nível", "Básico")));
+
+        alunos.insertOne(novoAluno);
         Document aluno = alunos.find().first();
         System.out.println(aluno);
         cliente.close();
